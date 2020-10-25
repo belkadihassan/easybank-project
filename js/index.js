adcf72f271b39a0fdd8373af6495d2e940f6f857
MedAfkir <afkir_mohamed_01@hotmail.fr>
2020-10-25 14:07:18 +0100

    Formatting docs and fix mistakes ;)

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
+$(document).ready(() => {
+  const width = $(window).width();
 
-const width = $(window).width()
-
-$(document).ready(()=>{
-    $('.bgImg,.phoneImg').delay(300).fadeIn(1000),
-    (function anime(){
-    $('.phoneImg').animate({
-        top:"+=10px",
-        right: "+=10px"
-    },1000,()=>{
-        $('.phoneImg').animate({
-            top:"-=10px",
-            right:"-=10px"
-        },1000,anime)
-    })
-})();
-})
+  $(".bgImg,.phoneImg").delay(300).fadeIn(1000),
+    (function anime() {
+      $(".phoneImg").animate(
+        {
+          top: "+=10px",
+          right: "+=10px",
+        },
+        1000,
+        () => {
+          $(".phoneImg").animate(
+            {
+              top: "-=10px",
+              right: "-=10px",
+            },
+            1000,
+            anime
+          );
+        }
+      );
+    })();
+});
